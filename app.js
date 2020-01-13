42bc6420eab0fd4a6cd07e1130bcee53b7c0233c
Matheus Duarte <mfduar8766@gmail.com>
2020-01-13 18:43:37 -0500

    Added dev dependency

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const GraphQLHTTP = require("express-graphql");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const schema = require("./schema");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongoURI = require("./config").MONGODB_URI;
 const app = express();
 
-const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
 
-mongoose.connect(mongoURI, options).catch(error => console.log('Error', error));
+mongoose.connect(mongoURI, options).catch(error => console.log("Error", error));
 
 mongoose.connection.once("open", () => {
   console.log("connected to DB.");
@@ -20,7 +22,8 @@ mongoose.connection.once("open", () => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/',
+app.use(
+  "/",
   GraphQLHTTP({
     schema,
     graphiql: true
